5e802aa754660857ac3166538d3e64b89f9ce357
CodeDead <admin@codedead.com>
2021-03-28 01:31:24 +0100

    * Added UI elements for the SettingsWindow

--- src/main/java/com/codedead/opal/controller/SettingsWindowController.java
+++ src/main/java/com/codedead/opal/controller/SettingsWindowController.java
@@ -1,5 +1,8 @@
 package com.codedead.opal.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,8 +14,14 @@ public final class SettingsWindowController {
 
     private static final Logger logger = LogManager.getLogger(SettingsWindowController.class);
 
+    @FXML
+    private CheckBox chbAutoUpdate;
+    @FXML
+    private ComboBox<String> cboLanguage;
+
     private SettingsController settingsController;
     private ResourceBundle translationBundle;
+    private Properties properties;
 
     /**
      * Initialize a new SettingsWindowController
@@ -41,13 +50,40 @@ public final class SettingsWindowController {
 
         this.settingsController = settingsController;
 
-        final Properties properties = settingsController.getProperties();
+        properties = settingsController.getProperties();
         final String languageTag = properties.getProperty("locale", "en-US");
 
         if (logger.isInfoEnabled()) {
             logger.info(String.format("Attempting to load the ResourceBundle for locale %s", languageTag));
         }
+
         final Locale locale = Locale.forLanguageTag(languageTag);
         translationBundle = ResourceBundle.getBundle("translations.OpalApplication", locale);
+
+        loadSettings();
+    }
+
+    /**
+     * Method that is invoked to initialize the FXML window
+     */
+    @FXML
+    private void initialize() {
+
+    }
+
+    /**
+     * Load all the settings into the UI
+     */
+    private void loadSettings() {
+        logger.info("Attempting to load all settings");
+        final boolean autoUpdate = Boolean.parseBoolean(properties.getProperty("autoUpdate", "true"));
+        final String locale = properties.getProperty("locale", "en-US");
+
+        chbAutoUpdate.setSelected(autoUpdate);
+        switch (locale.toLowerCase()) {
+            case "en-us" -> cboLanguage.getSelectionModel().select(0);
+            case "fr-fr" -> cboLanguage.getSelectionModel().select(1);
+            case "nl-nl" -> cboLanguage.getSelectionModel().select(2);
+        }
     }
 }
