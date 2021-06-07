ca64e06a31cbae8f25af19439f8d9ec5adbeb6ff
CodeDead <admin@codedead.com>
2021-06-07 03:12:49 +0200

    * Refactoring of AppImage creation

--- .AppImage/createAppImage.sh
+++ .AppImage/createAppImage.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+chmod a+x appimagetool-x86_64.AppImage
 BASEDIR=$(dirname "$0")
 PARENT_DIR="$(dirname "$BASEDIR")"
-cd $BASEDIR
 VAR="./appimagetool-x86_64.AppImage ${PARENT_DIR}/build/AppImage/Opal.AppDir ${PARENT_DIR}/build/AppImage/Opal-x86_64.AppImage"
 eval $VAR
+rm appimagetool-x86_64.AppImage
