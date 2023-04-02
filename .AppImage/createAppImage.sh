410e0ccaa8396d9fa2f2a1c2800b4706fb92aefe
CodeDead <admin@codedead.com>
2023-04-02 16:02:41 +0200

    Improved appimage build

--- .AppImage/createAppImage.sh
+++ .AppImage/createAppImage.sh
@@ -3,6 +3,6 @@ wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appim
 chmod a+x appimagetool-x86_64.AppImage
 BASEDIR=$(dirname "$0")
 PARENT_DIR="$(dirname "$BASEDIR")"
-VAR="./appimagetool-x86_64.AppImage ${PARENT_DIR}/build/AppImage/Opal.AppDir ${PARENT_DIR}/build/AppImage/Opal-x86_64-$1.AppImage"
+VAR="./appimagetool-x86_64.AppImage -n ${PARENT_DIR}/build/AppImage/Opal.AppDir ${PARENT_DIR}/build/AppImage/Opal-x86_64-$1.AppImage"
 eval $VAR
 rm appimagetool-x86_64.AppImage
