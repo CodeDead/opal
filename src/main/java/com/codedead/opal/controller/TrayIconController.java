6cf3593688e30c22deb066b362d3cb126c606219
CodeDead <admin@codedead.com>
2024-07-12 15:49:45 +0200

    feat: code refactoring

--- src/main/java/com/codedead/opal/controller/TrayIconController.java
+++ src/main/java/com/codedead/opal/controller/TrayIconController.java
@@ -98,8 +98,7 @@ public final class TrayIconController {
         logger.info("Displaying tray icon");
         if (trayIcon == null) {
             createTrayIcon();
-            if (trayIcon == null) {
-                logger.warn("TrayIcon cannot be null!");
+            if (!validTrayIcon()) {
                 return;
             }
         }
@@ -119,8 +118,8 @@ public final class TrayIconController {
      */
     public void hideTrayIcon() {
         logger.info("Hiding tray icon");
-        if (trayIcon == null) {
-            logger.warn("TrayIcon cannot be null!");
+
+        if (!validTrayIcon()) {
             return;
         }
 
@@ -128,5 +127,18 @@ public final class TrayIconController {
         tray.remove(trayIcon);
 
         trayIcon = null;
+    }
+
+    /**
+     * Check if the tray icon is valid
+     *
+     * @return True if the tray icon is valid, false otherwise
+     */
+    private boolean validTrayIcon() {
+        if (trayIcon == null) {
+            logger.warn("TrayIcon cannot be null!");
+            return false;
+        }
+        return true;
     }
 }
